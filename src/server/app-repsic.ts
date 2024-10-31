e2d4d8367343244a06590fd18030734326a623d1
manueldelapenna <manueldelapenna@gmail.com>
2024-10-31 12:14:28 -0300

    subo dmencu

--- src/server/app-repsic.ts
+++ src/server/app-repsic.ts
@@ -39,7 +39,7 @@ import { Request } from "rel-enc";
 
 import * as cookieParser from 'cookie-parser';
 
-const APP_DM_VERSION="#24-10-24";
+const APP_DM_VERSION="#24-10-31";
 
 interface Context extends procesamiento.Context{
   puede:object
