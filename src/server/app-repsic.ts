1b5753a8fa8cc1a7701a3f53d4612dd773e4ea08
manueldelapenna <manueldelapenna@gmail.com>
2024-11-13 15:07:32 -0300

    ajustes visuales

--- src/server/app-repsic.ts
+++ src/server/app-repsic.ts
@@ -39,7 +39,7 @@ import { Request } from "rel-enc";
 
 import * as cookieParser from 'cookie-parser';
 
-const APP_DM_VERSION="#24-11-08";
+const APP_DM_VERSION="#24-11-13";
 
 interface Context extends procesamiento.Context{
   puede:object
