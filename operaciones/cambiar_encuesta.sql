8fd684d7c780d0ec28399a047151e4760222456e
manueldelapenna <manueldelapenna@gmail.com>
2024-11-28 13:29:19 -0300

    agrego script para corregir encuesta cargada por error en otra

---
+++ operaciones/cambiar_encuesta.sql
@@ -0,0 +1,17 @@
+--script para pasar encuesta a otra (no intercambia) ej. contenido de 92201 a 93001 y blanqueo de 92201
+
+update tem set json_encuesta = (select json_encuesta from tem where operativo = 'REPSIC_242' and enc = '92201')
+where operativo = 'REPSIC_242' and enc = '93001';
+
+update tareas_tem set estado = 'D'
+where operativo = 'REPSIC_242' and enc = '93001' and tarea = 'ingr';
+
+
+update tareas_tem set estado = 'A'
+where operativo = 'REPSIC_242' and enc = '92201' and tarea = 'ingr';
+
+
+update tem set json_encuesta = null
+where operativo = 'REPSIC_242' and enc = '92201';
+
+--abrir encuestas y cambiar algun dato (ambas) y guardar para que se calcule nuevamente el resumen estado y demás
