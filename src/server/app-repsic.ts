8892875c6e8b9920652c9e7ec8cfca8cdfc91bfb
manueldelapenna <manueldelapenna@gmail.com>
2026-04-09 15:53:16 -0300

    subo dmencu

--- src/server/app-repsic.ts
+++ src/server/app-repsic.ts
@@ -42,7 +42,7 @@ import { Request } from "rel-enc";
 
 import * as cookieParser from 'cookie-parser';
 
-const APP_DM_VERSION="#31-04-09";
+const APP_DM_VERSION="#26-04-09";
 
 const menuMaps = {menuType:'maps', name:'mapa_puntos_gps', label:'mapa de puntos gps'};
 
